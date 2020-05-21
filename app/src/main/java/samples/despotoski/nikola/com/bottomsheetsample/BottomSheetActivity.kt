c8ca336c8e32aee435a6b27bd7488d9d791372be
Omer Iyioz <omer.iyioz@consultant.turkcell.com.tr>
2020-05-21 09:40:53 +0300

    refactoring

---
+++ app/src/main/java/samples/despotoski/nikola/com/bottomsheetsample/BottomSheetActivity.kt
@@ -0,0 +1,124 @@
+package samples.despotoski.nikola.com.bottomsheetsample
+
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.bottom_sheet_1.view.*
+import samples.despotoski.nikola.com.bottomsheetsample.databinding.ActivityBottomSheetBinding
+import java.util.*
+
+class BottomSheetActivity : AppCompatActivity() {
+    private var mBottomSheetBehavior: BottomSheetBehavior<*>? = null
+
+    lateinit var binding : ActivityBottomSheetBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_bottom_sheet)
+
+        setSupportActionBar(binding.toolbar)
+
+        setupBottomSheet1()
+
+        setupBottomSheet2()
+    }
+
+    private fun setupBottomSheet2() {
+        binding.showBottomSheet2.setOnClickListener {
+            val bottomSheetDialogFragment: BottomSheetDialogFragment = CustomBottomSheetDialogFragment()
+            bottomSheetDialogFragment.show(supportFragmentManager, bottomSheetDialogFragment.tag)
+        }
+    }
+
+    private fun setupBottomSheet1() {
+        binding.showBottomSheet1.setOnClickListener { //Let's peek it, programmatically
+            mBottomSheetBehavior?.peekHeight = binding.layoutBottomSheet1.textViewDragMe.height
+            binding.layoutBottomSheet1.textViewDragMe.requestLayout()
+        }
+
+        val recyclerView = binding.layoutBottomSheet1.recyclerview
+        recyclerView.layoutManager = LinearLayoutManager(this).apply {
+            isAutoMeasureEnabled = true
+        }
+        recyclerView.adapter = ApplicationAdapter(this, listApplications(this))
+        val parentThatHasBottomSheetBehavior = recyclerView.parent.parent as FrameLayout
+        mBottomSheetBehavior = BottomSheetBehavior.from(parentThatHasBottomSheetBehavior)
+        mBottomSheetBehavior?.let {
+            binding.stateText.setText(getStateAsString(it.state))
+            it.setBottomSheetCallback(object : BottomSheetCallback() {
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    binding.stateText.setText(getStateAsString(newState))
+                }
+
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                    binding.offsetText.text = getString(R.string.offset, slideOffset)
+                }
+            })
+        }
+    }
+
+    override fun onBackPressed() {
+        mBottomSheetBehavior?.let {
+            if (it.state != BottomSheetBehavior.STATE_HIDDEN) {
+                it.setState(BottomSheetBehavior.STATE_COLLAPSED)
+            } else {
+                super.onBackPressed()
+            }
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_bottom_sheet, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        fun getStateAsString(newState: Int): Int {
+            when (newState) {
+                BottomSheetBehavior.STATE_COLLAPSED -> return R.string.collapsed
+                BottomSheetBehavior.STATE_DRAGGING -> return R.string.dragging
+                BottomSheetBehavior.STATE_EXPANDED -> return R.string.expanded
+                BottomSheetBehavior.STATE_HIDDEN -> return R.string.hidden
+                BottomSheetBehavior.STATE_SETTLING -> return R.string.settling
+            }
+            return R.string.undefined
+        }
+
+        fun listApplications(context: Context): List<ApplicationInfo> {
+            val flags = PackageManager.GET_META_DATA
+            val installedApps: MutableList<ApplicationInfo> = ArrayList()
+            val pm = context.packageManager
+            val applications = pm.getInstalledApplications(flags)
+            for (appInfo in applications) {
+                if (appInfo.flags and ApplicationInfo.FLAG_SYSTEM != 1) {
+                    installedApps.add(appInfo)
+                }
+            }
+            return installedApps
+        }
+    }
+}
---
+++ app/src/main/java/samples/despotoski/nikola/com/bottomsheetsample/CustomBottomSheetDialogFragment.kt
@@ -0,0 +1,50 @@
+package samples.despotoski.nikola.com.bottomsheetsample
+
+import android.app.Dialog
+import android.view.View
+import android.widget.TextView
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+/**
+ * Created by Nikola D. on 2/25/2016.
+ */
+class CustomBottomSheetDialogFragment : BottomSheetDialogFragment() {
+    private var dialogOffsetText: TextView? = null
+    private var dialogStateText: TextView? = null
+    private val mBottomSheetBehaviorCallback: BottomSheetCallback = object : BottomSheetCallback() {
+        override fun onStateChanged(bottomSheet: View, newState: Int) {
+            setStateText(newState)
+            if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                dismiss()
+            }
+        }
+
+        override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            setOffsetText(slideOffset)
+        }
+    }
+
+    override fun setupDialog(dialog: Dialog, style: Int) {
+        val contentView = View.inflate(context, R.layout.dialog_bottom_sheet_2, null)
+        dialog.setContentView(contentView)
+        val layoutParams = (contentView.parent as View).layoutParams as CoordinatorLayout.LayoutParams
+        val behavior = layoutParams.behavior
+        if (behavior != null && behavior is BottomSheetBehavior<*>) {
+            behavior.setBottomSheetCallback(mBottomSheetBehaviorCallback)
+        }
+        dialogOffsetText = contentView.findViewById<View>(R.id.dialogOffsetText) as TextView
+        dialogStateText = contentView.findViewById<View>(R.id.dialogStateText) as TextView
+    }
+
+    private fun setOffsetText(slideOffset: Float) {
+        ViewCompat.postOnAnimation(dialogOffsetText!!) { dialogOffsetText!!.text = getString(R.string.offset, slideOffset) }
+    }
+
+    private fun setStateText(newState: Int) {
+        dialogStateText!!.setText(BottomSheetActivity.getStateAsString(newState))
+    }
+}
